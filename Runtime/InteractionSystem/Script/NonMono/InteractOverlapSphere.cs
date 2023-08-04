f034d3b752cccd5ff74ca690ff66154ba01a832d
Musab Kara <musabkara1990@gmail.com>
2023-08-04 20:20:05 +0300

    Update

---
+++ Runtime/InteractionSystem/Script/NonMono/InteractOverlapSphere.cs
@@ -0,0 +1,13 @@
+namespace InteractionSystem
+{
+    public class InteractOverlapSphere : Interactor
+    {
+        private float radius;
+        private float maxAngle;
+
+        public override IInteractable GetInteractable()
+        {
+            return null;
+        }
+    }
+}
